c5dbd42da83f385ff62fb40ca15cc7a04ef3f89f
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2021-06-24 12:46:58 +0100

    Using a check box to set state

--- src/Bookables/BookablesList.jsx
+++ src/Bookables/BookablesList.jsx
@@ -1,53 +1,88 @@
-import React, { useState } from 'react'
-import { bookables } from '../../static.json'
-import classes from './BookablesPage.module.css'
+import React, { useState, Fragment } from 'react'
+import { bookables, sessions, days } from '../../static.json'
 import { FaArrowRight } from 'react-icons/fa'
 
 export default function BookablesList() {
   const [group, setGroup] = useState('Kit')
   const bookablesInGroup = bookables.filter((b) => b.group === group)
   const [bookableIndex, setBookableIndex] = useState(0)
-  // const groups = [...new Set(bookables.map((b) => b.group))]
+  const groups = [...new Set(bookables.map((b) => b.group))]
+
+  const bookable = bookablesInGroup[bookableIndex]
+
+  const [hasDetails, setHasDetails] = useState(false)
 
   function nextBookable() {
     setBookableIndex((i) => (i + 1) % bookablesInGroup.length)
   }
-  function getUniqueValues(array, property) {
-    const propValues = array.map((element) => element[property])
-    const uniqueValues = new Set(propValues)
-    const uniqueValuesArray = [...uniqueValues]
-    return uniqueValuesArray
-  }
-  const groups = getUniqueValues(bookables, 'group')
 
   return (
-    <div>
-      <select value={group} onChange={(e) => setGroup(e.target.value)}>
-        {groups.map((g) => (
-          <option value={g} key={g}>
-            {g}
-          </option>
-        ))}
-      </select>
+    <Fragment>
+      <div>
+        <select value={group} onChange={(e) => setGroup(e.target.value)}>
+          {groups.map((g) => (
+            <option value={g} key={g}>
+              {g}
+            </option>
+          ))}
+        </select>
 
-      <ul className={classes.li}>
-        {bookablesInGroup.map((b, i) => (
-          <li
-            className={classes.li}
-            key={b.id}
-            className={i === bookableIndex ? 'selected' : null}>
-            <button className="btn" onClick={() => setBookableIndex(i)}>
-              {b.title}
-            </button>
-          </li>
-        ))}
-      </ul>
-      <p>
-        <button className="btn" onClick={nextBookable} autoFocus>
-          <FaArrowRight />
-          <span>Next</span>
-        </button>
-      </p>
-    </div>
+        <ul className="bookables items-list-nav">
+          {bookablesInGroup.map((b, i) => (
+            <li key={b.id} className={i === bookableIndex ? 'selected' : null}>
+              <button className="btn" onClick={() => setBookableIndex(i)}>
+                {b.title}
+              </button>
+            </li>
+          ))}
+        </ul>
+        <p>
+          <button className="btn" onClick={nextBookable} autoFocus>
+            <FaArrowRight />
+            <span>Next</span>
+          </button>
+        </p>
+      </div>
+
+      {bookable && (
+        <div className="bookable-details">
+          <div className="item">
+            <div className="item-header">
+              <h2>{bookable.title}</h2>
+              <span className="controls">
+                <label>
+                  <input
+                    type="checkbox"
+                    checked={hasDetails}
+                    onChange={() => setHasDetails((has) => !has)}
+                  />
+                  Show Details
+                </label>
+              </span>
+            </div>
+
+            <p>{bookable.notes}</p>
+
+            {hasDetails && (
+              <div className="item-details">
+                <h3>Availability</h3>
+                <div className="bookable-availability">
+                  <ul>
+                    {bookable.days.sort().map((d) => (
+                      <li key={d}>{days[d]}</li>
+                    ))}
+                  </ul>
+                  <ul>
+                    {bookable.sessions.map((s) => (
+                      <li key={s}>{sessions[s]}</li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      )}
+    </Fragment>
   )
 }
